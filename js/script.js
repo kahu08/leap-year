df7c6156066901db6cc876a946d6c49860627cff
kahu08 <peterkahurani@gmail.com>
2016-11-29 16:34:41 +0300

    code to test if it is divisible by 4

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@ $(document).ready(function() {
         var year = parseInt($('input#year').val());
         var result = leapYear(year);
         $("#result").text(result);
-        var leapYear = function(year) {
+
+    });
+    var leapYear = function(year) {
+        return false;
+    }
+    var leapYear = function(year) {
+        if (year % 4 === 0) {
+            return true;
+        } else {
             return false;
         }
-    });
+    }
 });
